c75b4968dea498d17786f38f608d227398c34bb4
TimurIbatulin <mel0072007@icloud.com>
2022-10-28 19:18:52 +0300

    Подготовка программы

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = new string [5];
+string [] arr = new string [5];
+for (int i=0; i<5; i++)
+{
+    Console.Write($"Введите {i+1} значение массива: ");
+    array[i] = Console.ReadLine();
+}
+int count=0;
+for (int j=0; j<5; j++)
+{
+    if (array[j].Length <=3)
+    {
+        arr[count]=array[j];
+        count++;
+    }
+}
+for (int h=0; h<count; h++)
+{
+    Console.Write($"{arr[h]} | ");
+}
